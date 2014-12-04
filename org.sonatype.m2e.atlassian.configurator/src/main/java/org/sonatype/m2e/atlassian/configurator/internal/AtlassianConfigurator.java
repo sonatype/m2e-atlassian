055f9a9f0cd21386e248f10d2acf1f71d9038587
Matthew Piggott <mpiggott@sonatype.com>
2014-12-04 12:25:36 -0500

    Patently stupid configuration

---
+++ org.sonatype.m2e.atlassian.configurator/src/main/java/org/sonatype/m2e/atlassian/configurator/internal/AtlassianConfigurator.java
@@ -0,0 +1,7 @@
+package org.sonatype.m2e.atlassian.configurator.internal;
+
+import org.eclipse.m2e.core.project.configurator.AbstractCustomizableLifecycleMapping;
+
+public class AtlassianConfigurator extends AbstractCustomizableLifecycleMapping {
+
+}
